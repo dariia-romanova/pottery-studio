15d442737cc6eee97d2ed17135911f47fc41834e
Dariia Romanova <dariia.romanova@gmail.com>
2023-07-06 13:43:20 +0200

    client: fetch hook

---
+++ src/client/hooks/useFetch.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const useFetch = (url: string) => {
+  const [data, setData] = useState<any>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchData = async () => {
+    setLoading(true);
+
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (e: any) {
+      setError(e);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => { 
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error, refetchData: fetchData }
+};
